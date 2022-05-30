dff84415ee9024ddc3db9957655343e8a99a9dd5
Jason Sutedjo <jason.s.coding@gmail.com>
2022-05-30 16:46:54 +0200

    checkpoint push

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,18 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  let resultArr = [];
+
+  for (let i = 0; i < mandatoryKeys.length; i++) {
+    manKey = mandatoryKeys[i];
+    // console.log(manKey);
+    for (let j = 0; j < apiData.length; j++) {
+      apiKey = apiData[j];
+      //console.log(apiKey);
+      if (apiData.has(manKey)) {
+        resultArr.push(apiData);
+      }
+    }
+    console.log(b);
+  }
 }
+
+// console.log(resultArr);
--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,24 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  let strAString = strA.split("");
+  let strBString = strB.split("");
+  let result = "";
+  let stringLength = undefined;
+
+  if (strAString < strBString) {
+    stringLength = strBString.length;
+  } else {
+    stringLength = strAString.length;
+  }
+
+  for (let i = 0; i < stringLength; i++) {
+    if (strAString[i] === "" || strAString[i] === undefined) {
+      result += strBString[i];
+    } else if (strBString[i] === "" || strBString[i] === undefined) {
+      result += strAString[i];
+    } else {
+      result += strAString[i] + strBString[i];
+    }
+  }
+
+  return result;
 }
